31b584a427a902e9fcdd45784feda4fe1c3bdb05
Baraa Aboseif <braaaboseif@gmail.com>
2026-03-09 18:32:33 +0300

    dun

--- execution/builtins/builtin_pwd.c
+++ execution/builtins/builtin_pwd.c
@@ -1,27 +1,42 @@
-
-// edit +test
-
 #include "builtin.h"
-
-// to make it same signature 
-// + dont broken function pointer
-
 int builtin_pwd(t_cmd *cmd, t_shell *shell)
 {
-    char *cwd;
+	char	*pwd;
+	char	mode;
+	int		i;
+	int		j;
 
-    (void)cmd;
-    (void)shell;
-    cwd = getcwd(NULL, 0);
-    if (!cwd)
-    {
-        perror("pwd");
-        return (1);
-    }
-    ft_putendl_fd(cwd , 1);
-    free(cwd);
-    return (0);
+	mode = 'L';
+	i = 1;
+	while (cmd->args[i] && cmd->args[i][0] == '-')
+	{
+		j = 1;
+		while (cmd->args[i][j])
+		{
+			if (cmd->args[i][j] == 'L' || cmd->args[i][j] == 'P')
+				mode = cmd->args[i][j];
+			else
+			{
+				ft_putendl_fd("pwd: invalid option", 2);
+                ft_putendl_fd("pwd: usage: pwd [-LP]", 2);
+
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	if (mode == 'P')
+		pwd = getcwd(NULL, 0);
+	else
+		pwd = get_env_value("PWD", shell->env);
+	if (!pwd)
+		pwd = getcwd(NULL, 0);
+	if (!pwd)
+		return (1);
+	ft_putendl_fd( pwd , 1);
+	if (mode == 'P')
+		free(pwd);
+	return (0);
 }
-
-
 
